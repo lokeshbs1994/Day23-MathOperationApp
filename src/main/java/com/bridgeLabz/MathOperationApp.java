34e35222d9835947025f574e6887ccfa160af71d
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-15 16:14:03 +0530

    ADDED: Pass lambda function to show result

--- src/main/java/com/bridgeLabz/MathOperationApp.java
+++ src/main/java/com/bridgeLabz/MathOperationApp.java
@@ -3,6 +3,10 @@ package com.bridgeLabz;
 @FunctionalInterface
 interface IMathFunction {
     int calculate(int a, int b);
+
+    static void printResult(int a, int b, String function, IMathFunction fobj) {
+        System.out.println("Result of " + function + " is " + fobj.calculate(a, b));
+    }
 }
 
 public class MathOperationApp {
@@ -22,5 +26,10 @@ public class MathOperationApp {
         System.out.println("Multiplication is " + multiply.calculate(6, 3));
         System.out.println("Division is " + divide.calculate(6, 3));
 
+        // passing Lambda Expression as Function parameter to print result using static function
+        IMathFunction.printResult(6, 3, "Addition", add);
+        IMathFunction.printResult(6, 3, "Subtraction", subtract);
+        IMathFunction.printResult(6, 3, "Multiplication", multiply);
+        IMathFunction.printResult(6, 3, "Division", divide);
     }
 }
